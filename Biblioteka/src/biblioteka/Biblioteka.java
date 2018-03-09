94b345a149d5e8b2cbe198ee22aac368ef23a71a
jankovicmaja <jankovic4716@fon.bg.ac.rs>
2018-03-09 14:45:49 +0100

    Uvedena logicka kontrola u klasu Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -5,29 +5,42 @@ import java.util.LinkedList;
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
-
+	
+	private LinkedList<Knjiga> knjige=new LinkedList<Knjiga>();
+	
+	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
-
+		if(knjiga==null || knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga je null ili vec postoji");
+		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
-
+		if(knjiga==null || !knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga je null ili ne postoji u biblioteci");
+		knjige.remove(knjiga);
 	}
 
 	@Override
 	public LinkedList<Knjiga> vratiSveKnjige() {
-		// TODO Auto-generated method stub
-		return null;
+		return knjige;
 	}
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<Knjiga> novaLista=new LinkedList<Knjiga>();
+		
+		if(naslov==null)
+			throw new RuntimeException("Naslov ne sme biti null vrednost");
+		
+		for (int i = 0; i < knjige.size(); i++) {
+			if(knjige.get(i).getNaslov().contains(naslov))
+				novaLista.add(knjige.get(i));
+		}
+		return novaLista;
 	}
+
 
 }
